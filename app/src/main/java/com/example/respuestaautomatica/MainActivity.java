b9d8665cfa9acb6d89504dbbaf964513f4d78c0b
magno309 <luisabrahamsanabriamelecio@gmail.com>
2021-02-21 13:42:23 -0600

    Actualización: - Agregados permisos al manifiesto - Agregaca clase para recibir llamadas

--- app/src/main/java/com/example/respuestaautomatica/MainActivity.java
+++ app/src/main/java/com/example/respuestaautomatica/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.respuestaautomatica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.respuestaautomatica.Servicios.CallReceiver;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtTelefono, txtMensaje;
+    Button btnGuardar;
+
+    public static String telefono, mensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtTelefono = findViewById(R.id.txtTelefonoActual);
+        txtMensaje = findViewById(R.id.txtMensaje);
+        btnGuardar = findViewById(R.id.btnGuardar);
+        btnGuardar.setOnClickListener(v -> {
+            //Aquí va el código
+            telefono = txtTelefono.getText().toString();
+            mensaje = txtMensaje.getText().toString();
+            CallReceiver callReceiver = new CallReceiver();
+            Toast.makeText(this, "Datos guardados!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
+
+// Referencias
+/*
+* https://programacion.net/articulo/enviar-_recibir_y_ver_el_historial_de_sms_con_android_1089
+* https://stackoverflow.com/questions/15563921/how-to-detect-incoming-calls-in-an-android-device
+*
+* */
---
+++ app/src/main/java/com/example/respuestaautomatica/Servicios/CallReceiver.java
@@ -0,0 +1,38 @@
+package com.example.respuestaautomatica.Servicios;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.util.Date;
+
+public class CallReceiver extends PhoneCallReceiver {
+    @Override
+    protected void onIncomingCallReceived(Context ctx, String number, Date start) {
+        Log.d("ICR", "Llamada recibida: " + number);
+    }
+
+    @Override
+    protected void onIncomingCallAnswered(Context ctx, String number, Date start) {
+
+    }
+
+    @Override
+    protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end) {
+
+    }
+
+    @Override
+    protected void onOutgoingCallStarted(Context ctx, String number, Date start) {
+
+    }
+
+    @Override
+    protected void onOutgoingCallEnded(Context ctx, String number, Date start, Date end) {
+
+    }
+
+    @Override
+    protected void onMissedCall(Context ctx, String number, Date start) {
+
+    }
+}
